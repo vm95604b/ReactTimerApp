4ac9a4f8188ccb1d7db93ea6075a2dce67eab29c
vm95604b <vm95604b@gmail.com>
2016-12-09 12:26:40 -0500

    Added tests Countdown

---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,39 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var Countdown = require('Countdown');
+
+describe("Countdown", () => {
+
+  it('Should exist', () => {
+    expect(Countdown).toExist();
+  });
+
+  describe('handleSetCountdown', () => {
+    it('Should set state to started and Countdown', (done) =>{
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(10);
+
+      expect(countdown.state.count).toBe(10);
+      expect(countdown.state.countdownStatus).toBe('started');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(9);
+        done();
+      },1001)
+    });
+    it('Should not allow the countdown to go negative', (done) => {
+        var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+        countdown.handleSetCountdown(1);
+
+        setTimeout(() => {
+          expect(countdown.state.count).toBe(0);
+          done();
+        }, 3000);
+    });
+
+  });
+});
