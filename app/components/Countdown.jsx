77c33b1c3486d4b6afad6c396f88ed595c0e9074
vm95604b <vm95604b@gmail.com>
2016-12-09 13:46:02 -0500

    Update status to stop when count reaches 0

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -11,7 +11,7 @@ var Countdown = React.createClass({
       countdownStatus: 'stopped'
     };
   },
-
+// ******LifeCycle Components******
   componentDidUpdate: function(prevProps, prevState) {
     if (this.state.countdownStatus !== prevState.countdownStatus) {
       switch (this.state.countdownStatus) {
@@ -28,12 +28,21 @@ var Countdown = React.createClass({
     }
   },
 
+  componentWillUnmount: function () {
+    clearInterval(this.timer);
+    this.timer = undefined;
+  },
+// *********************************
   startTimer: function () {
     this.timer = setInterval( ()=> {
       var newCount = this.state.count - 1;
       this.setState({
         count: newCount >= 0 ? newCount : 0
       });
+
+      if (newCount === 0) {
+        this.setState({countdownStatus: 'stopped'});
+      }
     }, 1000);
   },
 
